01606fd317d78e702909f9b2748fa1e4f8d236f8
pandavarapu-mdsol <pandavarapu-mdsol@users.noreply.github.com>
2024-08-02 15:47:08 +0000

    deploy: 0e9730350952c3e1cb7bd02eba6687b006e8d1fc

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-// as we are using more constants to export we shouldn't use default export. We have to use named exports
-
-export const CDN_URL =
-  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
-
-export const RES_LOGO2 = "https://www.licious.in/blog/wp-content/uploads/2022/06/chicken-hyderabadi-biryani-01.jpg";
-
-export const MENU_API = 'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
