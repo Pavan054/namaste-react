ccc08ae161c7a143ef4ba39755474797ac61cbdb
pandavarapu <pandavarapu@mdsol.com>
2024-08-02 23:06:54 +0530

    updated presets for babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     presets: [
-      ['@babel/preset-env', {targets: {node: 'defaults'}}], // changed current -> defaults
+      ['@babel/preset-env'], // ['@babel/preset-env', {targets: {node: 'current'}}],
       ['@babel/preset-react', {runtime: "automatic"}]
     ],
   };
